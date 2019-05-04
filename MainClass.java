c0c8b6c84b8504720f10ed41f6963b4ee5b1fe9e
Victor Antonio Ruiz Chambi - Laptop <victorRCH@P5GMD150.pe.intranet>
2019-05-03 20:49:27 -0500

    mensaje de inserción de la sexta linea

--- MainClass.java
+++ MainClass.java
@@ -6,5 +6,6 @@ public class MainClass{
 		System.out.println("Hola pequeño mundo del jenkins y github");
 		System.out.println("Hola pequeño mundo del jenkins y github y ngrok");
 		System.out.println("Hola pequeño mundo del jenkins y github y ngrok - quinta linea");
+		System.out.println("Agregamos una sexta linea y esta si es la vencida");
 	}
 }
