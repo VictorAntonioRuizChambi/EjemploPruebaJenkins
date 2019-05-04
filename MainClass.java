33816be4264db1a9785084f772f87ba82f843c0a
Victor Antonio Ruiz Chambi - Laptop <victorRCH@P5GMD150.pe.intranet>
2019-05-03 19:57:15 -0500

    se agrega una tercera linea de prueba

--- MainClass.java
+++ MainClass.java
@@ -3,5 +3,6 @@ public class MainClass{
 	public static void main(String[] args){
 		System.out.println("Hola pequeño mundo");
 		System.out.println("Hola pequeño mundo del jenkins");
+		System.out.println("Hola pequeño mundo del jenkins y github");
 	}
 }
